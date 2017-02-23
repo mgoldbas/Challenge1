cf356f70d4905ad65afba2bc26ddad9d200090d5
Maxwell Goldbas <mgoldbas@gmail.com>
2017-02-23 11:48:36 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+__author__ = "Maxwell Goldbas"
+
+"""
+This is the main structure of the untapt data challenge
+2017-02-23
+"""
---
+++ tests.py
@@ -0,0 +1,28 @@
+
+
+class IngestTestCases:
+    filename = 'test_cases.txt'
+
+    def __init__(self):
+        pass
+
+    def read_in_file(self):
+        """
+        ingest provided test file
+        :return:
+        """
+        pass
+
+    def set_test_cases_and_board(self):
+        """
+        set parameter for board
+        #TODO board must warn if robot goes outside map
+        :return:
+        """
+        pass
+
+    def set_test_cases(self):
+        """
+        there should be a test case for each robot
+        :return:
+        """
